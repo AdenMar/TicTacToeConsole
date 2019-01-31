678a7d02088c3ecb7f827ec9dbd9ec58a36fc974
AdenM <80mariyappa@gmail.com>
2019-01-31 06:27:27 -0500

    Final Version

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,87 +1,168 @@
 /**
  * TicTacToe class implements the interface
- * @author Aden Mariyappa
- * @date 1/28/19
+ * @author relkharboutly
+ * @date 1/5/2017
  */
 
-import java.util.Random;
-
 public class TicTacToe implements ITicTacToe {
-		 
-	   // The game board and the game status
-	   private static final int ROWS = 3, COLS = 3; // number of rows and columns
-	   private int[][] board = new int[ROWS][COLS]; // game board in 2D array
-	   private int setMoveRow;
-	   Random rand = new Random();
-	  
-	/**
-	 * clear board and set current player   
-	 */
-	public TicTacToe(){
-		
-	}
-	@Override
-	public void clearBoard() {
-		// TODO Auto-generated method stub
-		for (int row = 0; row < ROWS; ++row) {
-	         for (int col = 0; col < COLS; ++col) {
-	        	 board[row][col] = 0;
-	         }
-	      }
-	}
 
-	@Override
-	public void setMove(int player, int location) {
-		// TODO Auto-generated method stub
-		setMoveRow = (int)(location/3);
-		board[setMoveRow][location - (setMoveRow * 3)] = player;
-	}
+    // The game board and the game status
+    private static final int ROWS = 3, COLS = 3; // number of rows and columns
+    private int[][] board = new int[ROWS][COLS]; // game board in 2D array
+    private static int PLAY_COUNT;
+    /**
+     * clear board and set current player
+     */
+    public TicTacToe() {
+        PLAY_COUNT = 0;
+    }
 
-	@Override
-	public int getComputerMove() {
-		// TODO Auto-generated method stub
-		int n = rand.nextInt(8) + 0;
-		while (board[n - (setMoveRow * 3)][setMoveRow] != 0) {
-			n = rand.nextInt(8) + 0;
-		}
-		return n;
-	}
+    @Override
+    public void clearBoard() {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                board[i][j] = EMPTY;
+            }
+        }
+    }
 
-	@Override
-	public int checkForWinner() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	  /**
-	   *  Print the game board 
-	   */
-	   public  void printBoard() {
-	      for (int row = 0; row < ROWS; ++row) {
-	         for (int col = 0; col < COLS; ++col) {
-	            printCell(board[row][col]); // print each of the cells
-	            if (col != COLS - 1) {
-	               System.out.print("|");   // print vertical partition
-	            }
-	         }
-	         System.out.println();
-	         if (row != ROWS - 1) {
-	            System.out.println("-----------"); // print horizontal partition
-	         }
-	      }
-	      System.out.println();
-	   }
-	 
-	   /**
-	    * Print a cell with the specified "content" 
-	    * @param content either CROSS, NOUGHT or EMPTY
-	    */
-	   public  void printCell(int content) {
-	      switch (content) {
-	         case EMPTY:  System.out.print("   "); break;
-	         case NOUGHT: System.out.print(" O "); break;
-	         case CROSS:  System.out.print(" X "); break;
-	      }
-	   }
+    @Override
+    public void setMove(int player, int location) {
+        // TODO Auto-generated method stub
+        board[location / 3][location % 3] = player;
+
+        PLAY_COUNT++;
+    }
+
+    @Override
+    public int getComputerMove() {
+        // TODO Auto-generated method stub
+        int move = (int) (Math.random() * 9);
+
+        while (board[move / 3][move % 3] != EMPTY) {
+            move = (int) (Math.random() * 9);
+        }
+        return move;
+    }
+
+    public int checkForWinner() {
+        // TODO Auto-generated method stub
+
+        if ((board[0][0] == board[0][1]) && (board[0][1] == board[0][2])) {
+            if (board[0][0] == 1) {
+                return 2;
+            } else if (board[0][0] == 2) {
+                return 3;
+            }
+        }
+        // middle row
+        if ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2])) {
+            if (board[1][0] == 1) {
+                return 2;
+            } else if (board[1][0] == 2) {
+                return 3;
+            }
+        }
+
+        // bottom row
+        if ((board[2][0] == board[2][1]) && (board[2][1] == board[2][2])) {
+            if (board[2][0] == 1) {
+                return 2;
+            } else if (board[2][0] == 2) {
+                return 3;
+            }
+        }
+
+        // Check verticals
+
+        // left column
+        if ((board[0][0] == board[1][0]) && (board[1][0] == board[2][0])) {
+            if (board[0][0] == 1) {
+                return 2;
+            } else if (board[0][0] == 2) {
+                return 3;
+            }
+        }
+
+        // middle column
+        if ((board[0][1] == board[1][1]) && (board[1][1] == board[2][1])) {
+            if (board[0][1] == 1) {
+                return 2;
+            } else if (board[0][1] == 2) {
+                return 3;
+            }
+        }
+
+        // right column
+        if ((board[0][2] == board[1][2]) && (board[1][2] == board[2][2])) {
+            if (board[0][2] == 1) {
+                return 2;
+            } else if (board[0][2] == 2) {
+                return 3;
+            }
+        }
+        // Check diagonals
+        // one diagonal
+        if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
+            if (board[0][0] == 1) {
+                return 2;
+            } else if (board[0][0] == 2) {
+                return 3;
+            }
+        }
+
+        // the other diagonal
+        if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) {
+            if (board[0][2] == 1) {
+                return 2;
+            } else if (board[0][2] == 2) {
+                return 3;
+            }
+        }
+        if (PLAY_COUNT == 8){
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Print the game board
+     */
+    public void printBoard() {
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                printCell(board[row][col]); // print each of the cells
+                if (col != COLS - 1) {
+                    System.out.print("|");   // print vertical partition
+                }
+            }
+            System.out.println();
+            if (row != ROWS - 1) {
+                System.out.println("-----------"); // print horizontal partition
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Print a cell with the specified "content"
+     *
+     * @param content either CROSS, NOUGHT or EMPTY
+     */
+    public void printCell(int content) {
+        switch (content) {
+            case EMPTY:
+                System.out.print("   ");
+                break;
+            case NOUGHT:
+                System.out.print(" O ");
+                break;
+            case CROSS:
+                System.out.print(" X ");
+                break;
+        }
+    }
 
 }
